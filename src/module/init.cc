da86421bcf33d783ef287e23e4c57dfce8fa5be9
Perry Werneck <perry.werneck@gmail.com>
2026-06-23 21:08:21 -0300

    Updating build system and module loader.

--- src/module/init.cc
+++ src/module/init.cc
@@ -19,7 +19,7 @@
 
  #include <config.h>
  #include <udjat/module.h>
- #include <udjat/module/abstract.h>
+ #include <udjat/module.h>
  #include <udjat/module/atasmart.h>
  #include <udjat/tools/logger.h>
  #include <udjat/tools/properties.h>
